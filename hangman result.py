b13fe36a6a52fa28a684ddf01544ff62b2be341b
zeeshan13 <96335670+Zeeshan120@users.noreply.github.com>
2025-08-08 10:46:06 +0530

    Add files via upload

---
+++ hangman result.py
@@ -0,0 +1,19 @@
+Python 3.13.1 (tags/v3.13.1:0671451, Dec  3 2024, 19:06:28) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+============= RESTART: C:/Users/zeeshAN/OneDrive/Desktop/hangman.py ============
+Welcome to Hangman!
+_ _ _ _ _ 
+Guess a letter: p
+Good guess!
+_ p p _ _
+Guess a letter: a
+Good guess!
+a p p _ _
+Guess a letter: l
+Good guess!
+a p p l _
+Guess a letter: e
+Good guess!
+a p p l e
+Congratulations! You guessed the word!
